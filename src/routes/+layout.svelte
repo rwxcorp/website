44f3600f8ebbfef850b7d7b9095769b1b6d5ebfb
Ramon Rodrigues <ramonrwx@gmail.com>
2023-11-26 17:19:50 -0300

    add github sponsor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,3 +17,17 @@
 <div class="container mx-auto p-4">
   <slot />
 </div>
+
+<footer class="container mx-auto p-4 text-sm text-center">
+  <span class="dark:text-orange-600">
+    Supporting RwxCorp with
+    <a
+      aria-label="Github Sponsors"
+      class="text-orange-900 dark:text-orange-200 hover:text-orange-900 transition duration-300 ease-in-out"
+      href="https://github.com/sponsors/ramonrwx"
+      target="_blank"
+    >
+      Github Sponsors
+    </a>
+  </span>
+</footer>
