4edf4eaca1c171fb4d6c6f40aa774c36386cab6d
Ramon Rodrigues <ramonrwx@gmail.com>
2023-11-26 14:15:28 -0300

    add metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
   import '../app.postcss';
 </script>
 
+<svelte:head>
+  <title>RwXCorp</title>
+  <meta name="title" content="RwXCorp" />
+  <meta name="description" content="Explore RWXCorp open-source projects." />
+  <meta name="theme-color" content="#ff8100" />
+
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://rwxcorp.github.io/website" />
+  <meta property="og:title" content="RwXCorp" />
+  <meta property="og:description" content="Explore RWXCorp open-source projects." />
+</svelte:head>
+
 <slot />
