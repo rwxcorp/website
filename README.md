af5d9d743b83d64930832f3a598e93b2a35e99e7
Ramon Rodrigues <ramonrwx@gmail.com>
2023-11-26 13:40:17 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# rwxcorp
+
+This is the site that runs [rwxcorp.net](https://rwxcorp.net)
+
+This site is built on SvelteKit
+
+## Requirements
+
+- Node 20.0.0 or higher
+
+## Development
+
+First you `npm install`
+
+Then you `npm run dev` and visit `localhost:5173`.
---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+      precompress: false
+    }),
+    paths: {
+      base: dev ? '' : '/website'
+    }
+  },
+
+  preprocess: [vitePreprocess({})]
+};
+
+export default config;
