a8a8392f926fbc13c13d00bfcc2fe5532f418252
Ramon Rodrigues <ramonrwx@gmail.com>
2023-11-26 16:48:10 -0300

    add github repos component

---
+++ src/lib/components/GithubRepos.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  let repos = writable([]);
+
+  // fetch Github repositories data
+  async function fetchGithubRepos() {
+    try {
+      const response = await fetch('https://api.github.com/users/rwxcorp/repos');
+      const data = await response.json();
+      repos.set(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  onMount(fetchGithubRepos);
+</script>
+
+<div class="text-2xl mt-5 mb-5">
+  <span class="border-b-4 border-orange-600">Open source projects</span>
+</div>
+
+<ul class="flex flex-col gap-3 md:grid md:grid-cols-3 md:auto-rows-auto">
+  {#each $repos as repo (repo.id)}
+    <li class="rounded border-2 dark:border-none dark:bg-zinc-900 p-5">
+      <a
+        aria-label={repo.name}
+        class="text-[20px] text-orange-600 hover:text-orange-900 transition duration-300 ease-in-out"
+        href={repo.html_url}
+        target="_blank"
+      >
+        {repo.full_name}
+      </a>
+      <p>
+        {repo.description ? repo.description : 'No description for this repository'}
+      </p>
+    </li>
+  {/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import SocialMedia from '$lib/components/SocialMedia.svelte';
+  import GithubRepos from '$lib/components/GithubRepos.svelte';
 </script>
 
 <SocialMedia />
+<GithubRepos />
